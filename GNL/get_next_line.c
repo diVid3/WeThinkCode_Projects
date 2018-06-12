70708880f6cea3a659db41adc03d189a99b93f7c
diVid3 <egenis@student.wethinkcode.co.za>
2018-06-12 06:46:45 +0200

    Added multiplier to improve file build time

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 11:21:07 by egenis            #+#    #+#             */
-/*   Updated: 2018/06/08 13:09:04 by egenis           ###   ########.fr       */
+/*   Updated: 2018/06/12 06:46:04 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static	char	*ft_build_file(int fd)
+static	char	*ft_file_to_arr(int fd)
 {
 	char		*arr_tmp;
 	char		*arr_final;
 	char		*free_me;
+	size_t		multiplier;
 	ssize_t		read_bytes;
 
-	arr_final = ft_memalloc(sizeof(char) * BUFF_SIZE + 1);
-	if (!arr_final)
-		return (NULL);
-	read_bytes = read(fd, arr_final, BUFF_SIZE);
+	multiplier = BUFF_SIZE;
+	if (!(arr_final = ft_memalloc(sizeof(char) * multiplier + 1)))
+			return (NULL);
+	read_bytes = read(fd, arr_final, multiplier);
 	while (read_bytes > 0 && !(read_bytes < BUFF_SIZE))
 	{
 		free_me = arr_final;
-		arr_tmp = ft_memalloc(sizeof(char) * BUFF_SIZE + 1);
-		read_bytes = read(fd, arr_tmp, BUFF_SIZE);
+		arr_tmp = ft_memalloc(sizeof(char) * multiplier + 1);
+		read_bytes = read(fd, arr_tmp, multiplier);
 		arr_final = ft_strjoin(arr_final, arr_tmp);
 		ft_memdel((void **)(&free_me));
 		ft_memdel((void **)(&arr_tmp));
+		multiplier *= 2;
 	}
 	return (arr_final);
 }
@@ -55,7 +57,7 @@ int				get_next_line(const int fd, char **line)
 	(void)line;
 	if (fd < 0 || BUFF_SIZE <= 0)
 		return (-1);
-	str = ft_build_file(fd);
+	str = ft_file_to_arr(fd);
 	printf("%s", str);
 	return (1);
 }
