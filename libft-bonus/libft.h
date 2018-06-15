52a76e28f4cd86ba03593b23e3584bd0c901d54d
diVid3 <egenis@student.wethinkcode.co.za>
2018-06-15 07:00:02 +0200

    Bonus final

--- libft-bonus/libft.h
+++ libft-bonus/libft.h
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 09:27:47 by egenis            #+#    #+#             */
-/*   Updated: 2018/06/14 12:16:08 by egenis           ###   ########.fr       */
+/*   Updated: 2018/06/15 06:56:38 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 
-void				*ft_memccpy(void *dst, const void *src, int c,size_t n);
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
