ae878d74db2477d2d2d2f99ec904069f719652e0
diVid3 <egenis@student.wethinkcode.co.za>
2018-06-21 10:19:00 +0200

    Fixing GNL

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 17:16:57 by egenis            #+#    #+#             */
-/*   Updated: 2018/06/20 18:38:47 by egenis           ###   ########.fr       */
+/*   Updated: 2018/06/21 10:18:38 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ static	char	*ft_fd_to_line(int fd)
 
 	mem.nl_found = 0;
 	mem.spd = 2;
-	if (!(mem.arr_f = ft_memalloc(sizeof(char) * BUFF_SIZE + 1)))
+	if (!(mem.arr_l = ft_memalloc(sizeof(char) * BUFF_SIZE + 1)))
 		return (NULL);
-	mem.read_b = read(fd, arr_f, BUFF_SIZE);
+	mem.read_b = read(fd, arr_l, BUFF_SIZE);
 	while (mem.nl_found == 0 && mem.read_b == BUFF_SIZE)
 	{
-		mem.free = mem.arr_f;
+		mem.free = mem.arr_l;
 		mem.tmp = ft_memalloc(sizeof(char) * (BUFF_SIZE * mem.spd) + 1);
 		mem.cntr = 0;
 		while (mem.cntr < mem.spd)
@@ -29,25 +29,37 @@ static	char	*ft_fd_to_line(int fd)
 			mem.read_b = read(fd, tmp + (mem.read_b * mem.cntr), BUFF_SIZE);
 			++mem.cntr;
 		}
-		mem.arr_f = ft_strjoin(mem.arr_f, mem.tmp);
+		mem.arr_l = ft_strjoin(mem.arr_l, mem.tmp);
 		mem.nl_found = *(ft_strchr(mem.tmp, '\n'));
 		ft_memdel((void **)(&mem.free));
 		ft_memdel((void **)(&mem.tmp));
 		mem.spd *= mem.spd;
 	}
-	return (mem.arr_f);
+	return (mem.arr_l);
+}
+
+static	char	*ft_prep_arr(int fd, t_c_mem *cm)
+{
+	ft_memdel((void **)(&cm->arr_l));
+	//ft_memdel((void **)(&cm->old_store));
+	if (!(cm->arr_l = ft_fd_to_line(fd)))
+		return (NULL);
+	if (!cm->read_1ce)
+	{
+		cm->last_nl = ft_strrchr(arr_l, '\n');
+		cm->store = ft_strdup(last_nl + 1);
+	}
+
 }
 
 int				ft_controller(int fd, char **line)
 {
-	static	char	*arr_f = NULL;
-	static	char	*last_nl = NULL;
-	static	char	*store = NULL;
+	static	t_c_mem		cm = {NULL, NULL, NULL, 0};
 
-	arr_f = ft_fd_to_line(fd);
-	last_nl = ft_strrchr(arr_f, '\n');
+	arr_l = ft_fd_to_line(fd);
+	last_nl = ft_strrchr(arr_l, '\n');
 	if (last_nl)
-		store = ft_strdup(arr_f + 1);
+		store = ft_strdup(arr_l + 1);
 
 }
 
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 17:17:40 by egenis            #+#    #+#             */
-/*   Updated: 2018/06/19 18:22:17 by egenis           ###   ########.fr       */
+/*   Updated: 2018/06/21 08:35:47 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 typedef	struct	s_ac_mem;
 {
-	char		*arr_f;
+	char		*arr_l;
 	char		*tmp;
 	char		*free;
 	_Bool		nl_found;
@@ -26,6 +26,14 @@ typedef	struct	s_ac_mem;
 	size_t		spd;
 	size_t		cntr;
 }				t_ac_mem;
+
+typedef	struct	s_c_mem
+{
+	char		*arr_l;
+	char		*last_nl;
+	char		*store;
+	_Bool		read_1ce;
+}				t_c_mem;
 
 int				get_next_line(const int fd, char **line);
 
