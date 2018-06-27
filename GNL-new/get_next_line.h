b14720b47ac53b0d7b49e8f1df6bb8cfcb052628
diVid3 <egenis@student.wethinkcode.co.za>
2018-06-27 20:43:27 +0200

    Fixing GNL

--- GNL-new/get_next_line.h
+++ GNL-new/get_next_line.h
@@ -1,17 +1,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 8
 
 # include "libft.h"
-# define BUFF_SIZE 1
 
-typedef	struct	s_gnl_mem
-{
-	char		*arr_l;
-	char		*prev_arr_l;
-	ssize_t		read_b;
-	_Bool		swtch;
-}				t_gnl_mem;
-
-int	get_next_line(const int fd, char **line);
+int		get_next_line(const int fd, char **line);
 
 #endif
--- GNL-new/get_next_line_e.c
+++ GNL-new/get_next_line_e.c
@@ -1,60 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_b.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/25 07:09:30 by egenis            #+#    #+#             */
+/*   Updated: 2018/06/27 08:19:27 by egenis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 
-static int			ft_read_buffer(int const fd, char **line)
+static	void	ft_build_line(int fd, t_gnl_mem *m)
 {
-	char	*buff;
-	int		ret;
-	char	*tmp;
+	char		*tmp;
+	char		*prev_arr_l;
 
-	if (!(buff = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (-1);
-	ret = read(fd, buff, BUFF_SIZE);
-	if (ret > 0)
+	while (!ft_strchr(m->arr_l, '\n') && m->read_b)
 	{
-		buff[ret] = 0;
-		if (!(tmp = ft_strjoin(*line, buff)))
-			return (-1);
-		free(*line);
-		*line = tmp;
-		//printf("%s\n", *line);
-		free(buff);
+		prev_arr_l = m->arr_l;
+		tmp = ft_memalloc(sizeof(char) * BUFF_SIZE + 1);
+		m->read_b = read(fd, tmp, BUFF_SIZE);
+		m->arr_l = ft_strjoin(m->arr_l, tmp);
+		ft_memdel((void **)(&prev_arr_l));
+		ft_memdel((void **)(&tmp));
 	}
-	if (ret == 0)
-		free(buff);
-	return (ret);
 }
 
-int					get_next_line(int const fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	static char		*str;
-	char			*strn;
-	int				ret;
+	static t_gnl_mem	m = {NULL, NULL, 0, 0};
 
-	if ((!str && (!(str = (char*)malloc(sizeof(char) * BUFF_SIZE + 1))))
-			|| fd < 0 || BUFF_SIZE <= 0)
+	if (fd < 0 || BUFF_SIZE < 1 || !line)
 		return (-1);
-	strn = ft_strchr(str, '\n');
-	while (!strn)
-	{
-		ret = ft_read_buffer(fd, &str);
-		if (ret == 0 && !ft_strlen(str))
-			return (0);
-		if (ret == 0)
-			ft_strcat(str, "\n");
-		if (ret < 0)
+	if (!m.arr_l)
+		if (!(m.arr_l = ft_memalloc(sizeof(char) * BUFF_SIZE + 1))
+			|| ((m.read_b = read(fd, m.arr_l, BUFF_SIZE)) == -1))
 			return (-1);
-		else
-			strn = ft_strchr(str, '\n');
-	}
-	if (!(*line = ft_strsub(str, 0, ft_strlen(str) - ft_strlen(strn))))
+	if (!ft_strchr(m.arr_l, '\n') && m.read_b)
+		ft_build_line(fd, &m);
+	*line = ft_strsub(m.arr_l, 0, ft_strclen(m.arr_l, '\n'));
+	m.prev_arr_l = m.arr_l;
+	if (m.read_b)
 	{
-		//printf("%s\n", *line);
-		return (-1);
+		// What if ft_strchr() returns NULL???
+		m.arr_l = ft_strdup(ft_strchr(m.arr_l, '\n') + 1);
+		ft_memdel((void **)(&m.prev_arr_l));
+		return (1);
 	}
-	ft_strcpy(str, strn + 1);
-	return (1);
+	if (!m.read_b && !m.swtch && ft_strlen(*line))
+	{
+		m.swtch = 1;
+		return (1);
+	}
+	return (0);
 }
 
 void			print_test(char *line)
