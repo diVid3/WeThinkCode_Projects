a30965f068712620a482b02234265e109c7016a7
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-03 20:55:09 +0200

    Added ft_atoi_base

---
+++ Exam-prep/level3/ft_atoi_base.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int		ft_strlen(char *str)
+{
+	size_t	cntr;
+
+	cntr = 0;
+	while (str[cntr])
+		++cntr;
+	return (cntr);
+}
+
+// Temporarily made this for hex, will adjust later.
+int ft_atoi_base(const char *str, int str_base)
+{
+	//char c[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'};
+	int		ans;
+	int		tmp;
+	int		cntr;
+	int		cntr2;
+
+	ans = 0;
+	tmp = 0;
+	cntr3 = 0;
+	cntr = ft_strlen(str);
+	while (cntr > 0)
+	{
+		cntr2 = 0;
+		while (cntr2 < cntr3)
+		{
+			tmp *= 16;
+			++cntr2;
+		}
+		if (str[cntr] >= '0' && str[cntr] <= '9')
+			tmp += str[cntr] - 48;
+		if (str[cntr] >= 'A' && str[cntr] <= 'F')
+			tmp += str[cntr] - 55;
+		if (str[cntr] >= 'a' && str[cntr] <= 'f')
+			tmp += str[cntr] - 87;
+		ans += tmp;
+		--cntr;
+		++cntr3;
+	}
+}
+
+int main(void)
+{
+	return (0);
+}
