3c80ce72116be986dbe601b17571d8091cb4df63
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-12 08:07:42 +0200

    Added Stuff

--- Exam-prep/level4/brainfuck.c
+++ Exam-prep/level4/brainfuck.c
@@ -1,5 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brainfuck.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/11 08:26:40 by egenis            #+#    #+#             */
+/*   Updated: 2018/07/11 08:37:56 by egenis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdlib.h>
+
+void	brainf(int *cntr, char *str, char **ptr)
+{
+	while (str[*cntr])
+	{
+
+		++(*cntr);
+	}
+}
 
 int		main(int ac, char **av)
 {
@@ -7,6 +28,8 @@ int		main(int ac, char **av)
 	unsigned int			cntr = 0;
 	unsigned char			*ptr = arr;
 
+	if (ac != 2)
+		write(1, "\n", 1);
 	if (ac != 2)
 		return (0);
 	while (av[1][cntr])
@@ -21,8 +44,8 @@ int		main(int ac, char **av)
 			--(*ptr);
 		if (av[1][cntr] == '.')
 			write(1, ptr, 1);
-		if (av[1][cntr] == '[' && !*ptr)
-
+		if (av[1][cntr] == '[' && *ptr)
+			brainf(&cntr, av[1], &ptr);
 		++cntr;
 	}
 	return (0);
--- Filler/src/ft_get_map.c
+++ Filler/src/ft_get_map.c
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 16:53:21 by egenis            #+#    #+#             */
-/*   Updated: 2018/07/09 17:31:19 by egenis           ###   ########.fr       */
+/*   Updated: 2018/07/11 16:17:24 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int			ft_get_map(t_data *data)
 	if ((get_next_line(0, &line)) == -1)
 		return (-1);
 	ft_memdel((void **)(&line));
-	if (data->map_skp_2line)
+	if (!data->map_skp_2line)
 		if ((get_next_line(0, &line)) == -1)
 			return (-1);
-	if (data->map_skp_2line)
+	if (!data->map_skp_2line)
 		ft_memdel((void **)(&line));
 	data->map_skp_2line = 1;
 	return (0);
