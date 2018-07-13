b51b38a12dc3208578916b91cd7e425f193e5079
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-13 10:33:35 +0200

    Added func to alloc arr2d + to del arr2d

--- Filler/inc/filler.h
+++ Filler/inc/filler.h
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 13:32:26 by egenis            #+#    #+#             */
-/*   Updated: 2018/07/13 10:32:29 by egenis           ###   ########.fr       */
+/*   Updated: 2018/07/13 10:33:27 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 # include "../lib/libft/get_next_line.h"
 
 void		ft_del_arr2d(char **arr, size_t row_count);
+
 int			ft_get_plyr_num(t_data *data);
 int			ft_get_map_size(t_data *data);
+
 char		**ft_alloc_arr2d(size_t rows, size_t cols);
 
 #endif
