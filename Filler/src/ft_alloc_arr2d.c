98968305b73c134942092ccc0de9d4dca314c1f1
Evert GENIS <egenis@e4r15p5.wethinkcode.co.za>
2018-07-13 10:31:18 +0200

    Added func to alloc arr2d + to del arr2d

---
+++ Filler/src/ft_alloc_arr2d.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_alloc_arr2d.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/13 07:31:43 by egenis            #+#    #+#             */
+/*   Updated: 2018/07/13 10:29:33 by egenis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/filler.h"
+#include <stdio.h>
+
+void		ft_del_arr2d(char **arr, size_t row_count)
+{
+	size_t		cntr;
+
+	if (!row_count || !arr)
+		return ;
+	cntr = 0;
+	while (cntr < row_count && arr[cntr])
+	{
+		free(arr[cntr]);
+		arr[cntr] = NULL;
+		++cntr;
+	}
+	free(arr);
+	arr = NULL;
+	return ;
+}
+
+char		**ft_alloc_arr2d(size_t rows, size_t cols)
+{
+	char		**arr;
+	size_t		cntr_row;
+
+	if (!rows || !cols)
+		return (NULL);
+	if ((arr = malloc(sizeof(char *) * rows + 1)) == NULL)
+		return (NULL);
+	cntr_row = 0;
+	while (cntr_row < rows)
+	{
+		if ((arr[cntr_row] = malloc(sizeof(char) * cols)) == NULL)
+		{
+			ft_del_arr2d(arr, rows);
+			return (NULL);
+		}
+		++cntr_row;
+	}
+	arr[rows] = NULL;
+	return (arr);
+}
+
+void		print_arr2d(char **arr, size_t row_count)
+{
+	size_t		cntr;
+
+	if (!row_count || !arr)
+		return ;
+	cntr = 0;
+	while (cntr < row_count && arr[cntr])
+	{
+		printf("%s\n", arr[cntr]);
+		++cntr;
+	}
+	return ;
+}
+
+int			main(int ac, char **av)
+{
+	char	**arr;
+
+	(void)ac;
+	(void)av;
+	arr = ft_alloc_arr2d(3, 6);
+
+	arr[0][0] = 'H';
+	arr[0][1] = 'E';
+	arr[0][2] = 'L';
+	arr[0][3] = 'L';
+	arr[0][4] = 'O';
+	arr[0][5] = '\0';
+
+	arr[1][0] = 'T';
+	arr[1][1] = 'H';
+	arr[1][2] = 'E';
+	arr[1][3] = 'R';
+	arr[1][4] = 'E';
+	arr[1][5] = '\0';
+
+	arr[2][0] = 'B';
+	arr[2][1] = 'U';
+	arr[2][2] = 'D';
+	arr[2][3] = 'D';
+	arr[2][4] = 'Y';
+	arr[2][5] = '\0';
+
+	print_arr2d(arr, 3);
+	ft_del_arr2d(arr, 3);
+	//sleep(60);
+	return (0);
+}
--- Filler/src/ft_del_arr2d.c
+++ Filler/src/ft_del_arr2d.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_del_arr2d.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/13 07:31:53 by egenis            #+#    #+#             */
+/*   Updated: 2018/07/13 07:44:50 by egenis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/filler.h"
-#include <stdio.h>
 
 void		ft_del_arr2d(char **arr, size_t row_count)
 {
@@ -17,58 +28,4 @@ void		ft_del_arr2d(char **arr, size_t row_count)
 	free(arr);
 	arr = NULL;
 	return ;
-}
-
-size_t		ft_count_wrds(char *s, char c)
-{
-	size_t		cntr;
-	size_t		wrd_count;
-	_Bool		swtch;
-	t_uchar		ch;
-
-	cntr = 0;
-	wrd_count = 0;
-	swtch = 0;
-	ch = (t_uchar)c;
-	while (s[cntr])
-	{
-		if (s[cntr] != ch && swtch == 0)
-			++wrd_count;
-		if (s[cntr] != ch)
-			swtch = 1;
-		if (s[cntr] == ch)
-			swtch = 0;
-		++cntr;
-	}
-	return (wrd_count);
-}
-
-void		print_arr2d(char **arr, size_t row_count)
-{
-	size_t		cntr;
-
-	if (!row_count || !arr)
-		return ;
-	cntr = 0;
-	while (cntr < row_count && arr[cntr])
-	{
-		printf("%s\n", arr[cntr]);
-		++cntr;
-	}
-	return ;
-}
-
-int			main(int ac, char **av)
-{
-	size_t	wrd_count;
-	char	**arr;
-
-	(void)ac;
-	wrd_count = ft_count_wrds(av[1], ' ');
-	arr = ft_strsplit(av[1], ' ');
-	printf("The word count for av[1] is %zu\n", wrd_count);
-	print_arr2d(arr, 6);
-	ft_del_arr2d(arr, 6);
-	//sleep(60);
-	return (0);
 }
