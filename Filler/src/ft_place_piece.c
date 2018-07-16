36e636f9c978361ebd6295835465ef7278bed5d6
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-16 21:35:46 +0200

    Need to fix opst-kill

--- Filler/src/ft_place_piece.c
+++ Filler/src/ft_place_piece.c
@@ -12,57 +12,6 @@
 
 #include "../inc/filler.h"
 
-/*
-static _Bool	ft_can_place(t_input *data, int map_x, int map_y)
-{
-	int		cntr_row;
-	int		cntr_col;
-	_Bool	swtch;
-
-	swtch = 0;
-	if (map_x + data->piece_rows > data->map_rows || 
-		map_y + data->piece_cols > data->map_cols)
-		return (0);
-	cntr_row = 0;
-	while (cntr_row < data->piece_rows)
-	{
-		cntr_col = 0;
-		while (cntr_col < data->piece_cols)
-		{
-			if (data->player_num == 2 && swtch == 0 &&
-				data->piece[cntr_row][cntr_col] == '*' &&
-				(data->map[map_x + cntr_row][map_y + cntr_col] == 'x' ||
-				data->map[map_x + cntr_row][map_y + cntr_col] == 'X'))
-			{
-				swtch = 1;
-				++cntr_col;
-			}
-			if (data->player_num == 2 && swtch == 1 &&
-				data->piece[cntr_row][cntr_col] == '*' &&
-				(data->map[map_x + cntr_row][map_y + cntr_col] == 'x' ||
-				data->map[map_x + cntr_row][map_y + cntr_col] == 'X'))
-				return (0);
-			if (data->player_num == 1 && swtch == 0 &&
-				data->piece[cntr_row][cntr_col] == '*' &&
-				(data->map[map_x + cntr_row][map_y + cntr_col] == 'o' ||
-				data->map[map_x + cntr_row][map_y + cntr_col] == 'O'))
-			{
-				swtch = 1;
-				++cntr_col;
-			}
-			if (data->player_num == 1 && swtch == 1 &&
-				data->piece[cntr_row][cntr_col] == '*' &&
-				(data->map[map_x + cntr_row][map_y + cntr_col] == 'o' ||
-				data->map[map_x + cntr_row][map_y + cntr_col] == 'O'))
-				return (0);
-			++cntr_col;
-		}
-		++cntr_row;
-	}
-	return (1);
-}
-*/
-
 static _Bool	ft_can_place_p1(t_input *data, int map_row, int map_col)
 {
 	int		overlaps;
@@ -81,6 +30,8 @@ static _Bool	ft_can_place_p1(t_input *data, int map_row, int map_col)
 		{
 			if ((data->map[map_row + p_row][map_col + p_col] == 'o' ||
 				data->map[map_row + p_row][map_col + p_col] == 'O') &&
+				(data->map[map_row + p_row][map_col + p_col] != 'x' ||
+				data->map[map_row + p_row][map_col + p_col] != 'X') &&
 				data->piece[p_row][p_col] == '*')
 				++overlaps;
 			++p_col;
@@ -108,6 +59,8 @@ static _Bool	ft_can_place_p2(t_input *data, int map_row, int map_col)
 		{
 			if ((data->map[map_row + p_row][map_col + p_col] == 'x' ||
 				data->map[map_row + p_row][map_col + p_col] == 'X') &&
+				(data->map[map_row + p_row][map_col + p_col] != 'o' ||
+				data->map[map_row + p_row][map_col + p_col] != 'O') &&
 				data->piece[p_row][p_col] == '*')
 				++overlaps;
 			++p_col;
@@ -117,6 +70,30 @@ static _Bool	ft_can_place_p2(t_input *data, int map_row, int map_col)
 	return ((overlaps == 1) ? 1 : 0);
 }
 
+static void		ft_add_node_back(t_move **head, int map_row, int map_col)
+{
+	t_move		*node;
+	t_move		*tmp;
+
+	node = malloc(sizeof(t_move));
+	node->row = map_row;
+	node->col = map_col;
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = node;
+		node->next = NULL;
+	}
+}
+
+/*
 static void		ft_add_node(t_move **head, int map_row, int map_col)
 {
 	t_move		*node;
@@ -130,6 +107,7 @@ static void		ft_add_node(t_move **head, int map_row, int map_col)
 		node->next = *head;
 	*head = node;
 }
+*/
 
 static t_move		*ft_make_move_list(t_input *data)
 {
@@ -146,10 +124,10 @@ static t_move		*ft_make_move_list(t_input *data)
 		{
 			if (data->player_num == 1)
 				if (ft_can_place_p1(data, cntr_row, cntr_col) == 1)
-					ft_add_node(&head, cntr_row, cntr_col);
+					ft_add_node_back(&head, cntr_row, cntr_col);
 			if (data->player_num == 2)
 				if (ft_can_place_p2(data, cntr_row, cntr_col) == 1)
-					ft_add_node(&head, cntr_row, cntr_col);
+					ft_add_node_back(&head, cntr_row, cntr_col);
 			++cntr_col;
 		}
 		++cntr_row;
@@ -175,6 +153,7 @@ void			ft_place_piece(t_input *data)
 	t_move		*move;
 
 	move = ft_make_move_list(data);
+	sleep(1);
 	if (move)
 		ft_putnbr(move->row);
 	else
