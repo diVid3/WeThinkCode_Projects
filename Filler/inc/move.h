3220074e7165c0d423da6458d28cb83049d422b8
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-16 11:18:28 +0200

    Added ft_place_piece.c

--- Filler/inc/move.h
+++ Filler/inc/move.h
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   filler.h                                           :+:      :+:    :+:   */
+/*   move.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/09 13:32:26 by egenis            #+#    #+#             */
-/*   Updated: 2018/07/16 10:58:43 by egenis           ###   ########.fr       */
+/*   Created: 2018/07/16 10:28:28 by egenis            #+#    #+#             */
+/*   Updated: 2018/07/16 10:58:56 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLER_H
-# define FILLER_H
+#ifndef MOVE_H
+# define MOVE_H
 
-# include "../lib/libft/libft.h"
-# include "input.h"
-# include "move.h"
-
-int				ft_get_player_num(t_input *data);
-int				ft_get_map_size(t_input *data);
-int				ft_get_map(t_input *data);
-int				ft_get_piece_size(t_input *data);
-int				ft_get_piece(t_input *data);
+typedef struct		s_move
+{
+	int				row;
+	int				col;
+	struct s_move	*next;
+}					t_move;
 
 #endif
--- Filler/src/ft_place_piece.c
+++ Filler/src/ft_place_piece.c
@@ -6,16 +6,43 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 07:06:10 by egenis            #+#    #+#             */
-/*   Updated: 2018/07/16 08:29:01 by egenis           ###   ########.fr       */
+/*   Updated: 2018/07/16 11:18:04 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/filler.h"
 
-int				ft_place_piece(t_input *data)
+static _Bool	ft_can_place(t_input *data, int map_row, int map_col)
 {
+}
+
+static void		ft_add_node(t_move **head, int map_row, int map_col)
+{
+}
+
+static void		ft_make_move_list(t_input *data)
+{
+	t_move		*head;
 	size_t		cntr_row;
 	size_t		cntr_col;
 
+	head = NULL;
+	cntr_row = 0;
+	while (cntr_row < data->map_rows)
+	{
+		cntr_col = 0;
+		while (cntr_col < data->map_cols)
+		{
+			if (ft_can_place(data, cntr_row, cntr_col) == 0)
+				continue;
+			else
+				ft_add_node(&head, cntr_row, cntr_col);
+			++cntr_col
+		}
+		++cntr_row;
+	}
+}
 
+void			ft_place_piece()
+{
 }
