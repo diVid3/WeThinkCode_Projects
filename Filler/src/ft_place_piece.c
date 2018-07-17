aa5522ece8b9739f4df66e43b6f21aced707987a
diVid3 <egenis@student.wethinkcode.co.za>
2018-07-17 17:25:24 +0200

    Need to test placement fix

--- Filler/src/ft_place_piece.c
+++ Filler/src/ft_place_piece.c
@@ -6,7 +6,7 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 07:06:10 by egenis            #+#    #+#             */
-/*   Updated: 2018/07/17 15:21:17 by egenis           ###   ########.fr       */
+/*   Updated: 2018/07/17 17:24:47 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,39 @@ static void		ft_no_moves(void)
 	ft_putchar('\n');
 }
 
-static void		ft_print_move(int row, int col)
+static void		ft_print_move(t_move *move)
 {
-	ft_putnbr(row);
+	ft_putnbr(move->row);
 	ft_putchar(' ');
-	ft_putnbr(col);
+	ft_putnbr(move->col);
 	ft_putchar('\n');
+}
+
+static t_move	*ft_optimal_move(t_move *head)
+{
+	t_move		*tmp;
+	t_move		*opt_move;
+	int			rating;
+
+	tmp = head;
+	opt_move = tmp;
+	rating = 2147483647;
+	while (tmp)
+	{
+		if (tmp->rating < rating)
+		{
+			opt_move = tmp;
+			rating = tmp->rating;
+		}
+		tmp = tmp->next;
+	}
+	return (opt_move);
 }
 
 void			ft_place_piece(t_input *data)
 {
-	int			row;
-	int			col;
-	int			rating;
 	t_move		*moves;
+	t_move		*opt_move;
 
 	moves = ft_make_move_list(data);
 	//sleep(1);
@@ -43,19 +62,7 @@ void			ft_place_piece(t_input *data)
 	if (moves == NULL)
 		return ;
 	ft_rate_moves(data, moves);
-	rating = moves->rating;
-	//dprintf(2, "rating is %d\n", rating);
-	while (moves)
-	{
-		if (moves->rating <= rating)
-		{
-			row = moves->row;
-			col = moves->col;
-			rating = moves->rating;
-			//dprintf(2, "rating is %d\n", rating);
-		}
-		moves = moves->next;
-	}
-	ft_print_move(row, col);
+	opt_move = ft_optimal_move(moves);
+	ft_print_move(opt_move);
 	ft_free_list(moves);
 }
