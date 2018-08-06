0e403afdc97067d965d0be482b93eb329111b160
diVid3 <egenis@student.wethinkcode.co.za>
2018-08-06 10:56:54 +0200

    Improved ft_do_ops.c

--- Algorithms_1/push_swap/src/checker/ft_do_ops.c
+++ Algorithms_1/push_swap/src/checker/ft_do_ops.c
@@ -1,11 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_do_ops.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/06 07:10:34 by egenis            #+#    #+#             */
+/*   Updated: 2018/08/06 10:56:29 by egenis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/checker/checker.h"
-#include <stdio.h>
 
-// Remember to free line inside the operation functions.
-
-static int		ft_do_ops2(t_stacks *stacks, char **line)
+static int		ft_pick_op(t_stacks *stacks, char **line)
 {
-	if (ft_strcmp(*line, "ra") == 0)
+	if (ft_strcmp(*line, "sa") == 0)
+		ft_swap(stacks, 0);
+	else if (ft_strcmp(*line, "sb") == 0)
+		ft_swap(stacks, 1);
+	else if (ft_strcmp(*line, "ss") == 0)
+		ft_swap(stacks, 2);
+	else if (ft_strcmp(*line, "pa") == 0)
+		ft_push(stacks, 0);
+	else if (ft_strcmp(*line, "pb") == 0)
+		ft_push(stacks, 1);
+	else if (ft_strcmp(*line, "ra") == 0)
 		ft_rotate(stacks, 0);
 	else if (ft_strcmp(*line, "rb") == 0)
 		ft_rotate(stacks, 1);
@@ -18,10 +37,7 @@ static int		ft_do_ops2(t_stacks *stacks, char **line)
 	else if (ft_strcmp(*line, "rrr") == 0)
 		ft_r_rotate(stacks, 2);
 	else
-	{
-		ft_putstr_fd("Error\n", 2);
 		return (-1);
-	}
 	return (0);
 }
 
@@ -31,23 +47,14 @@ int				ft_do_ops(t_stacks *stacks)
 
 	while (get_next_line(0, line))
 	{
-		if (ft_strcmp(*line, "sa") == 0)
-			ft_swap(stacks, 0);
-		else if (ft_strcmp(*line, "sb") == 0)
-			ft_swap(stacks, 1);
-		else if (ft_strcmp(*line, "ss") == 0)
-			ft_swap(stacks, 2);
-		else if (ft_strcmp(*line, "pa") == 0)
-			ft_push(stacks, 0);
-		else if (ft_strcmp(*line, "pb") == 0)
-			ft_push(stacks, 1);
-		else if (ft_do_ops2(stacks, line) == -1)
-				return (-1);
+		if (ft_pick_op(stacks, line) == -1)
+		{
+			ft_putstr_fd("Error\n", 2);
+			ft_memdel((void **)(line));
+			return (-1);
+		}
+		else
+			ft_memdel((void **)(line));
 	}
-	return (0);
-}
-
-int		main(int ac, char **av)
-{
 	return (0);
 }
