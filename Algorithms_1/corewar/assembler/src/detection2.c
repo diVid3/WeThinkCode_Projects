b209422f6dfd995c1fe790611e31060328d54d40
diVid3 <egenis@student.wethinkcode.co.za>
2018-09-16 21:29:57 +0200

    building is_func, need subroutines

--- Algorithms_1/corewar/assembler/src/detection2.c
+++ Algorithms_1/corewar/assembler/src/detection2.c
@@ -35,7 +35,7 @@ int		is_label_chars(char c)
 	return (0);
 }
 
-int		is_label_return(char *str_no_cmmnt, char **arr, int cntr)
+int		free_return(char *str_no_cmmnt, char **arr, int cntr)
 {
 	ft_memdel((void **)(&str_no_cmmnt));
 	ft_del_matrix((void **)arr, cntr);
@@ -49,23 +49,23 @@ int		is_label(char *str)
 	char	*str_no_cmmnt;
 	char	**arr;
 
-	str_no_cmmnt = strcdup(str, '#');
+	str_no_cmmnt = strcdup(str, COMMENT_CHAR);
 	arr = split(str_no_cmmnt);
 	cntr = 0;
 	while (arr[cntr])
 		++cntr;
 	if (cntr != 1)
-		if (is_label_return(str_no_cmmnt, arr, cntr) == 0)
+		if (free_return(str_no_cmmnt, arr, cntr) == 0)
 			return (0);
 	cntr2 = -1;
 	while (arr[0][++cntr2] && arr[0][cntr2] != LABEL_CHAR)
 		if (is_label_chars(arr[0][cntr2]) == 0)
-			if (is_label_return(str_no_cmmnt, arr, cntr) == 0)
+			if (free_return(str_no_cmmnt, arr, cntr) == 0)
 				return (0);
 	if (arr[0][cntr2] != LABEL_CHAR || arr[0][++cntr2] != '\0')
-		if (is_label_return(str_no_cmmnt, arr, cntr) == 0)
+		if (free_return(str_no_cmmnt, arr, cntr) == 0)
 			return (0);
-	is_label_return(str_no_cmmnt, arr, cntr);
+	free_return(str_no_cmmnt, arr, cntr);
 	return (1);
 }
 
@@ -73,8 +73,76 @@ int		is_label_op(char *str)
 {
 }
 
+/*
+** This function treats non-nil as true, and nil as false, it thus returns
+** the valid operation's opcode upon validation.
+*/
+
+int		is_func_valid(char *func)
+{
+	int		cntr;
+
+	cntr = 0;
+	while (++cntr <= 16)
+		if (ft_strcmp(func, g_op_tab[cntr].name) == 0)
+			return (cntr);
+	return (0);
+}
+
+int		is_arg_types_valid(int opcode, char **arr)
+{
+	is_reg();
+	is_dir();
+	is_ind();
+}
+
+int		is_args_valid(char *func, char *args)
+{
+	int		cntr;
+	int		field_cntr;
+	int		field_valid;
+	int		opcode;
+	char	**arr;
+
+	opcode = is_func_valid(func);
+	arr = ft_strsplit(args, SEPARATOR_CHAR);
+	field_cntr = -1;
+	field_valid = 0;
+	while (++field_cntr < 3)
+		if (g_op_tab[opcode].params[field_cntr] > 0)
+			++field_valid;
+	cntr = 0;
+	while (arr[cntr])
+		++cntr;
+	if (field_valid != cntr)
+		if (free_return(NULL, arr, cntr) == 0)
+			return (0);
+	is_arg_types_valid(opcode, arr);
+	return (1);
+}
+
 int		is_func(char *str);
 {
+	int		cntr;
+	char	*str_no_cmmnt;
+	char	**arr;
+
+	str_no_cmmnt = strdup(str, COMMENT_CHAR);
+	arr = split(str_co_cmmnt);
+	cntr = 0;
+	while (arr[cntr])
+		++cntr;
+	if (cntr != 2)
+		if (free_return(str_no_cmmnt, arr, cntr) == 0)
+			return (0);
+	if (is_func_valid(arr[0]) == 0)
+		if (free_return(str_no_cmmnt, arr, cntr) == 0)
+			return (0);
+	if (is_args_valid(arr[0], arr[1]) == 0)
+		if (free_return(str_no_cmmnt, arr, cntr) == 0)
+			return (0);
+	free_return(str_no_cmmnt, arr, cntr);
+	return (1);
 }
 
 /*
@@ -107,4 +175,3 @@ int		main(void)
 	return (0);
 }
 */
-
