b12e7a5b19131ce9dd81c98c9d33b37deee8e834
diVid3 <egenis@student.wethinkcode.co.za>
2018-10-18 08:48:44 +0200

    Fixed bugs

--- Web_1/camagru/config/setup.php
+++ Web_1/camagru/config/setup.php
@@ -1,13 +1,18 @@
 <?php
-include("database.php");
+include ("database.php");
 
 function createDB() {
+    global $DB_SERVER;
+    global $DB_USER;
+    global $DB_PASSWORD;
+    global $DB_DATABASE_NAME;
+    global $DB_DSN;
+
     try {
-        $PDO = new PDO("mysql:host=$DB_SERVER", $DB_USERNAME, $DB_PASSWORD);
+        $PDO = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $PDO->query("CREATE DATABASE IF NOT EXISTS $DB_DATABASE_NAME");
         $PDO = NULL;
-        echo "DB made!\n";
     }
     catch (PDOexception $e) {
         echo "Failed to create database $DB_DATABASE_NAME" . PHP_EOL;
