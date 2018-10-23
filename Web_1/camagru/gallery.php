ae377e7a48481531fc9e35cb516efdbf325849ef
diVid3 <egenis@student.wethinkcode.co.za>
2018-10-23 17:47:41 +0200

    kk

---
+++ Web_1/camagru/gallery.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'Ducks everywhere!' . '<br>';
+?>
--- Web_1/camagru/index.php
+++ Web_1/camagru/index.php
@@ -23,6 +23,40 @@ if (file_exists('initialized.txt') == false) {
     <script src="main.js"></script>
 </head>
 <body>
-    hi
+    <div class="float-header">
+        <!-- <form action="login.php" method="POST">
+            Username:
+            <input type="text" name="username">
+            Password:
+            <input type="text" name="password">
+            <input type="submit" name="submit">
+        </form> -->
+        <div class="box-1">
+            <div style="text-align:center;">
+                <a href='gallery.php'>Gallery</a>
+            </div>
+        </div>
+        <div class="box-2">
+            <div style="text-align:center;">
+                <a href='sign_up.php'>Take a Photo</a>
+            </div>
+        </div>
+        <div class="box-3">
+            <div style="text-align:center;">
+                <a href='sign_up.php'>Sign Up</a>
+            </div>
+        </div>
+        <div class="box-4">
+            <div style="text-align:center;">
+                <a href='login.php'>Log In</a>
+            </div>
+        </div>
+    </div>
+    <div id="body-push">
+    </div>
+    <div style="font-size:500%;">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta laborum perspiciatis aut, quibusdam recusandae eveniet fuga facere eos. Natus esse rem ipsa temporibus, enim earum deleniti officiis culpa qui autem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias impedit, numquam facilis aliquam magni excepturi veniam fuga at, expedita magnam est vel veritatis eligendi, error dicta eos beatae repellendus ut.
+    </div>
 </body>
 </html>
