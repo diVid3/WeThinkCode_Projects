5e6c4566871cee7980d5883e9a46d39fb8613433
diVid3 <egenis@student.wethinkcode.co.za>
2018-11-14 18:39:46 +0200

    need to finish mergeImages

--- Web_1/camagru/processSavePicture.php
+++ Web_1/camagru/processSavePicture.php
@@ -4,9 +4,14 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . '/inc/errors.php');
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/inc/connect.php');
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/inc/initialize.php');
 if (isset($_SESSION['username']) == false && isset($_SESSION['password']) == false && isset($_SESSION['email']) == false) {
-    header("Location: http://127.0.0.1:8080/take_photo_sign_in.php");
+    // header("Location: http://127.0.0.1:8080/take_photo_sign_in.php");
     exit;
 }
 
-
+function mergeImages($imageEncoded, $stickerPath) {
+    file_put_contents('picture.png', base64_decode($imageEncoded));
+    $picture = imagecreatefrompng('picture.png');
+    $sticker = imagecreatefrompng($stickerPath);
+    imagecopymerge($picture, $sticker, 0, 0, 0, 0);
+}
 ?>
