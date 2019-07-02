53599f528e37ed2a2b5ce8c5176d80d94d773c17
diVid3 <egenis@student.wethinkcode.co.za>
2019-07-02 18:48:13 +0200

    refactored, fixed pom

---
+++ Java/swingy/src/test/java/za/co/wethinkcode/MainTest.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue( true );
+  }
+}
